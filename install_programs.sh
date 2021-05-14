d7f4433d2591c3ba33d46aab59c6a75321ef251b
vitoryeso <vitoryeso@outlook.com>
2021-05-14 00:08:33 -0300

    xDDD

--- install_programs.sh
+++ install_programs.sh
@@ -1,2 +1,10 @@
-pacman -S feh transmission deepin-screenshot noto-fonts-emoji ttf-joypixels telegram-desktop discord gsimplecal arch-install-scripts pcmanfm-gtk3
+pacman -S feh transmission deepin-screenshot noto-fonts-emoji ttf-joypixels telegram-desktop discord gsimplecal arch-install-scripts pcmanfm-gtk3 zsh deepin-gtk-theme lxappearance-gtk3
+
+# lxappearance-gtk3 is for switch gtk theme easily
+
+# clone fast syntax highlighting for zsh
+git clone https://github.com/zdharma/fast-syntax-highlighting.git ~/
+
+# 0.5 version of neovim
+yay -S neovim-nightly-git
 
