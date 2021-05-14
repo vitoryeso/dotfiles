1bdeefac08b85308e670d66d7f15d1d1fb37d6b8
vitoryeso <vitoryeso@outlook.com>
2021-05-13 22:19:25 -0300

    audio programs and i3wm update

--- audio_plugins.sh
+++ audio_plugins.sh
@@ -1,3 +1,9 @@
 sudo pacman -S fluidsynth audacity calf caps carla dpf-plugins dragonfly-reverb drumgizmo eq10q geonkick guitarix helm ir.lv2 jack_capture ninjas2 noise-repellent samplv1 setbfree sherlock.lv2 x42-plugins zita-rev1
 
-yay -S sonic-visualizer-hg
+yay -S sonic-visualizer-hg bitrot-git
+
+# ladspa plugins for voice effects
+# autotalent plugin for autotune
+yay -S autotalent
+# gvst gsnap plugin for autotune and pitch correction
+yay -S gvst-plugins
--- install_programs.sh
+++ install_programs.sh
@@ -1,2 +1,2 @@
-pacman -S feh transmission deepin-screenshot noto-fonts-emoji ttf-joypixels telegram-desktop discord
+pacman -S feh transmission deepin-screenshot noto-fonts-emoji ttf-joypixels telegram-desktop discord gsimplecal arch-install-scripts pcmanfm-gtk3
 
